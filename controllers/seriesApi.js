7d1bf765fbc40651b91986dee68dfab1a306bbaa
csilla <csilla.bukki@gmail.com>
2018-03-24 20:51:12 -0700

    Create and Delete API

--- controllers/seriesApi.js
+++ controllers/seriesApi.js
@@ -33,3 +33,44 @@ module.exports.showMovie = function(req, res){
     }
     
 }
+
+
+module.exports.createMovie = function(req, res){
+    Mov.create({
+        name: req.body.name,
+        seasons: req.body.seasons,
+        years: req.body.years,
+        genres: req.body.genres,
+        netflixlink: req.body.netflixlink
+    }, function(err, movie){
+        if(err){
+            res.status(400);
+            res.json(err);
+        } else {
+            res.status(201);
+            res.json(movie);
+        }
+    });
+}
+//test
+//{"name" : "The puma", "seasons": 15, "years": "2000-", "netflixlink":"https://www.puma.org", "genres": [{"genre":"TV Shows"}, {"genre": //"Docuseries"}]}
+
+module.exports.deleteMovie = function(req, res){
+    var movieid = req.params.movieid;
+    if (movieid){
+        Mov.findByIdAndRemove(movieid)
+           .exec(
+                function(err, movie){
+                    if(err){
+                        res.status(404);
+                        res.json(err);
+                        return;
+                    }
+                    res.status(204);
+                    res.json(null);
+                });
+    } else {
+        res.status(404);
+        res.json({"message":"no id"});
+    } 
+}
--- routes/indexApi.js
+++ routes/indexApi.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var netflixseries = require('../controllers/seriesApi');
 
 router.get('/netflixseries', netflixseries.moviesAll);
-//router.post('/questionanswers', questionanswers.questionanswersCreate);
+router.post('/netflixseries', netflixseries.createMovie);
 router.get('/netflixseries/:movieid', netflixseries.showMovie);
 //router.put('/questionanswers/:questionanswerid', questionanswers.questionanswersUpdate);
 //router.patch('/questionanswers/:questionanswerid', questionanswers.questionanswersReplace);
-//router.delete('/questionanswers/:questionanswerid', questionanswers.questionanswersDelete);
+router.delete('/netflixseries/:movieid', netflixseries.deleteMovie);
 
 module.exports = router;
