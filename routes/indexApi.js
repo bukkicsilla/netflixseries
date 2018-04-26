94f7565c244b66469491273b0462d9ec5be1fdce
csilla <csilla.bukki@gmail.com>
2018-04-26 13:29:06 -0700

    replacement in Postman

--- routes/indexApi.js
+++ routes/indexApi.js
@@ -6,7 +6,7 @@ router.get('/netflixseries', netflixseries.moviesAll);
 router.post('/netflixseries', netflixseries.createMovie);
 router.get('/netflixseries/:movieid', netflixseries.showMovie);
 router.put('/netflixseries/:movieid', netflixseries.updateMovie);
-//router.patch('/questionanswers/:questionanswerid', questionanswers.questionanswersReplace);
+router.patch('/netflixseries/:movieid', netflixseries.replaceInfo);
 router.delete('/netflixseries/:movieid', netflixseries.deleteMovie);
 
 module.exports = router;
